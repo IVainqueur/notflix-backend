0e4a130ad755b412abbf77c58f84a0876ed5b912
IVainqueur <ishimvainqueur@gmail.com>
2022-07-08 16:14:48 +0200

    Nothing important

--- routes/pup.js
+++ routes/pup.js
@@ -162,7 +162,7 @@ const goojara_search = async (searchQuery) => {
                 .then(res => res.text())
                 .then(res => { data = res })
                 .catch(e => {
-                    data = {code: "#Error", error: e.toString()}
+                    data = {code: "#Error", error: e.stack}
                 })
             return data
         }, searchQuery)
